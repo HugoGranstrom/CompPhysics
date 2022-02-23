9477dd1ca4e7e4c25e7579009d12d3de12b49414
Jagolen <jakob.golen@gmail.com>
2022-02-23 12:41:55 +0100

    funcs done

--- HomExercise/Project1/test.nim
+++ HomExercise/Project1/test.nim
@@ -1,13 +1,33 @@
 import utils
 import math
 
-proc exactSolution*(theta: float, b, r_max, u0, E: float): float =
+proc exactSolution*(b, r_max, u0, E: float): float =
   let r_min = b / sqrt(1 - u0 / E) # calculate this by finding root of 1 - b^2 / r^2 - V / E
   if E < u0:
     result = arctan(b / r_max / sqrt(1 - b*b / (r_max*r_max) - u0 / E)) - arctan(b / r_min / sqrt(1 - b*b / (r_min*r_min) - u0 / E)) + arcsin(1.0) - arcsin(b / r_max)
     result *= 2
 
-proc f(x: float): float =
-    return x^2
 
-echo gaussQuad(f, 0, 3, 3)
+
+proc thetafunc(u0,r_max,E,b: float) =
+
+    let r_min = b / sqrt(1 - u0 / E)
+
+    proc f1(x: float): float =
+        return 1/sqrt(x-b)*1/sqrt(1-abs(x-b)-u0/E)
+
+    proc f2(x: float): float =
+        return 1/sqrt(x-b)*1/sqrt(1-abs(x-b))
+
+    let a1 = b^2/r_min^2 + b
+    let a2 = 1+b
+    let b1 = b^2/r_max^2 + b
+    let b2 = b^2/r_max^2 + b
+    let int1 = gaussQuad(f1,a1,b1,5)
+    let int2 = gaussQuad(f2,a2,b2,5)
+    let exact = exactSolution(b,r_max,u0,E)
+    let res = int1+int2
+    echo res
+    echo exact
+    echo r_min
+thetafunc(-1,1,-2,0.1)
