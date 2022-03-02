2c139c82a1dae657b5e547da5109c34b1b7d476e
Jagolen <jakob.golen@gmail.com>
2022-03-02 15:43:34 +0100

    We got graphs!

--- HomExercise/Project3/main.nim
+++ HomExercise/Project3/main.nim
@@ -1,4 +1,4 @@
-import math, random, sequtils
+import math, random, sequtils, ggplotnim, numericalnim
 
 type
   Lattice* = object
@@ -7,6 +7,7 @@ type
     J*, B*, T*: float 
 
 const kb = 8.617e-5
+const J = 0.000189574
 
 proc `[]`*(lattice: Lattice, row, col: int): float =
   # use modulus to wrap around periodic BC. 
@@ -47,10 +48,30 @@ proc calcHamiltonian*(lattice: Lattice): float =
       pairsSum += calcHamiltonian(lattice, row, col)
   result += -lattice.J * pairsSum
 
-proc ising*(lattice: var Lattice, steps: int) =
+proc M_calc(lattice: Lattice): float =
+  return sum(lattice.data)/lattice.data.len.toFloat
+
+proc Heat_calc(E: float, eSquare: float, T: float): float =
+  return (eSquare - E*E)/(kb*T*T)
+
+proc Sus_calc(M: float, mSquare: float, T:float): float =
+  return (mSquare-M*M)/(kb*T)
+
+proc Cumulant(mSquare: float, m4: float): float =
+  return 1 - m4/(3*mSquare*mSquare)
+
+proc mean(x: seq[float]): float =
+  return sum(x)/x.len.float
+
+proc ising*(lattice: var Lattice): (float, float, float, float, float) =
   var hamiltonian = calcHamiltonian(lattice)
   var dE = 1e10
   var iters: int
+  var eTot: seq[float]
+  var mTot: seq[float]
+  var msquareTot: seq[float]
+  var esquareTot: seq[float]
+  var m4Tot: seq[float]
   while abs(dE) > 1e-10 or iters < 100:
     iters += 1
     var innerHamiltonian = hamiltonian
@@ -65,23 +86,78 @@ proc ising*(lattice: var Lattice, steps: int) =
           if r > exp(-dEinner / (lattice.T * kb)):
             #echo "Rejected! ", dEinner
             lattice.flip(row, col) # flip back
+          else:
+            innerHamiltonian = newHamiltonian
         else:
           innerhamiltonian = newHamiltonian
+    if iters > 50:
+      let M = M_calc(lattice)
+      let msquare = M*M
+      let m4 = msquare*msquare
+      let esquare = innerHamiltonian*innerHamiltonian
+      eTot.add innerHamiltonian
+      mTot.add M
+      msquareTot.add msquare
+      esquareTot.add esquare
+      m4Tot.add m4
+    
     dE = lattice.calcHamiltonian() - hamiltonian
     #echo dE
     hamiltonian = innerHamiltonian
-
+  let M = mean(mTot)
+  let msquare = mean(msquareTot)
+  let m4 = mean(m4Tot)
+  let e = mean(eTot)
+  let esquare = mean(esquareTot)
   echo "Iterations: ", iters
+  return (M,msquare,m4,e,esquare)
+  
+
+  
+
+
+
 
 proc main() =
-  let J = 0.000189574
-  let T = 2.2
-  let B = 0.0
-  randomize(10)
-  var lattice = newLattice(10, 10, J, B, T)
-  echo lattice.calcHamiltonian
-  ising(lattice, 100)
-  echo lattice.calcHamiltonian
+  var heattot: seq[float]
+  var sustot: seq[float]
+  var cumultot: seq[float]
+  var mTot: seq[float]
+  let cs = linspace(1.0,7.0,15)
+  for c in cs:
+    #let c = 1.0
+    let T = J*c/kb
+    #let c = kb*T/J
+    
+    echo "c = ", c
+    let B = 0.0
+    
+    randomize(111)
+    var lattice = newLattice(10, 10, J, B, T)
+    echo lattice.calcHamiltonian
+    let (M,msquare,m4,e,esquare) = ising(lattice)
+    echo lattice.calcHamiltonian
+    let specHeat = Heat_calc(e,esquare,T)
+    let sus = Sus_calc(M,msquare,T)
+    let cumul = Cumulant(msquare,m4)
+
+    echo "Specific heat = ", specHeat
+    echo "Suseptibility = ", sus
+    echo "Cumulant = ", cumul
+    echo "Magnetization = ", M
+    heattot.add specHeat
+    sustot.add sus
+    cumultot.add abs(cumul)
+    mTot.add abs(M)
+  let df1 = seqsToDf({"c":cs,"magnetization":mTot})
+  let df2 = seqsToDf({"c":cs,"suseptibility":sustot})
+  let df3 = seqsToDf({"c":cs, "cumulant":cumultot})
+  let df4 = seqsToDf({"c":cs,"specific heat":heattot})
+  ggplot(df1, aes("c","magnetization")) + geom_line() + ggsave("mag.png")
+  ggplot(df2, aes("c","suseptibility")) + geom_line() + ggsave("sus.png")
+  ggplot(df3, aes("c","cumulant")) + geom_line() + ggsave("cumul.png")
+  ggplot(df4, aes("c","specific heat")) + geom_line() + ggsave("heat.png")
+
 
   
 main()
