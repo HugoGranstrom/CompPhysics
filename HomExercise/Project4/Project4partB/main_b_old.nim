bad1835a03b554b7dc2083631e1ee3edc5c6181a
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-03-18 08:42:44 +0100

    heatbath data

--- HomExercise/Project4/Project4partB/main_b_old.nim
+++ HomExercise/Project4/Project4partB/main_b_old.nim
@@ -1,26 +1,149 @@
-import math, random, sequtils, ggplotnim, numericalnim, algorithm
+import math, random, sequtils, ggplotnim, numericalnim
 import locks, taskpools, cpuinfo
-import ../utils
-const J = -0.000189574
+
+type
+  Lattice* = object
+    width*, height*: int
+    data*: seq[float]
+    J*, B*, T*: float 
+
+const kb = 8.617e-5
+const J = 0.000189574
+
+proc `[]`*(lattice: Lattice, row, col: int): float =
+  # use modulus to wrap around periodic BC. 
+  let modRow = (row + lattice.height - 1) mod (lattice.height - 1)
+  let modCol = (col + lattice.width - 1) mod (lattice.width - 1)
+  #echo modRow, " ", modCol
+  result = lattice.data[(lattice.width-1)*modRow + modCol]
+
+proc `[]=`*(lattice: var Lattice, row, col: int, val: float) =
+  let modRow = (row + lattice.height - 1) mod (lattice.height - 1)
+  let modCol = (col + lattice.width - 1) mod (lattice.width - 1)
+  lattice.data[(lattice.width-1)*modRow + modCol] = val
+
+proc flip*(lattice: var Lattice, row, col: int) =
+  lattice[row, col] = lattice[row, col] * -1.0
+
+proc newLattice*(height, width: int, J, B, T: float, rnd: var Rand): Lattice =
+  result.height = height
+  result.width = width
+  result.J = J
+  result.B = B
+  result.T = T
+  result.data = newSeqWith[float]((height-1)*(width-1), rnd.sample([-1.0, 1.0]))
+
+
+proc calcHamiltonian(lattice: Lattice, row, col: int): float =
+  let current = lattice[row, col]
+  #result += current * lattice[row-1, col] #
+  result += current * lattice[row+1, col]
+  #result += current * lattice[row, col-1] #
+  result += current * lattice[row, col+1]
+
+proc calcHamiltonian*(lattice: Lattice): float =
+  result = -lattice.B * sum(lattice.data)
+  var pairsSum: float
+  for row in 0 ..< lattice.height - 1:
+    for col in 0 ..< lattice.width - 1:
+      pairsSum += calcHamiltonian(lattice, row, col)
+  result += 2 * -lattice.J * pairsSum
+
+proc calcProbability*(lattice: Lattice, row, col: int): float =
+  let beta = 1 / (kb * lattice.T)
+  let s = lattice[row-1, col] + lattice[row, col-1] + lattice[row, col+1] + lattice[row+1, col]
+  let e = exp(2 * J * beta * s)
+  result = e / (1 + e)
+
+proc M_calc(lattice: Lattice): float =
+  return sum(lattice.data)/lattice.data.len.toFloat
+
+proc Heat_calc(E: float, eSquare: float, T: float): float =
+  return (eSquare - E*E)/(kb*T*T)
+
+proc Sus_calc(M: float, mSquare: float, T:float): float =
+  return (mSquare-M*M)/(kb*T)
+
+proc Cumulant(mSquare: float, m4: float): float =
+  return 1 - m4/(3*mSquare*mSquare)
+
+proc mean(x: seq[float]): float =
+  return sum(x)/x.len.float
+
+proc ising*(lattice: var Lattice, rnd: var Rand): (float, float, float, float, float) =
+  var hamiltonian = calcHamiltonian(lattice)
+  var dE = 1e10
+  var iters: int
+  var eTot: seq[float]
+  var mTot: seq[float]
+  var msquareTot: seq[float]
+  var esquareTot: seq[float]
+  var m4Tot: seq[float]
+  while abs(dE) > 1e-10 or iters < 100:
+    iters += 1
+    var innerHamiltonian = hamiltonian
+    for row in 0 ..< lattice.height - 1:
+      for col in 0 ..< lattice.width - 1:
+        let p = calcProbability(lattice, row, col)
+        let r = rnd.rand(1.0)
+        if r < p:
+          lattice[row, col] = 1
+        else:
+          lattice[row, col] = -1
+        #[ lattice.flip(row, col)
+        let newHamiltonian = calcHamiltonian(lattice)
+        let dEinner = newHamiltonian - innerhamiltonian
+        if dEinner > 0:
+          let r = rnd.rand(1.0)
+          #if iters < 10: echo -dEinner / (lattice.T * kb)
+          if r > exp(-dEinner / (lattice.T * kb)):
+            #echo "Rejected! ", dEinner
+            lattice.flip(row, col) # flip back
+          else:
+            innerHamiltonian = newHamiltonian
+        else:
+          innerhamiltonian = newHamiltonian ]#
+    if iters > 50:
+      innerHamiltonian = calcHamiltonian(lattice)
+      let M = M_calc(lattice)
+      let msquare = M*M
+      let m4 = msquare*msquare
+      let esquare = innerHamiltonian*innerHamiltonian
+      eTot.add innerHamiltonian
+      mTot.add M
+      msquareTot.add msquare
+      esquareTot.add esquare
+      m4Tot.add m4
+    
+    dE = innerHamiltonian - hamiltonian
+    
+    hamiltonian = innerHamiltonian
+  let M = mean(mTot)
+  let msquare = mean(msquareTot)
+  let m4 = mean(m4Tot)
+  let e = mean(eTot)
+  let esquare = mean(esquareTot)
+  
+  return (M,msquare,m4,e,esquare)
+  
 
 var modLock: Lock
 
-proc thread_func*(task_id: int, c_len: int, cs: ptr UncheckedArray[float], avgHeat: ptr UncheckedArray[float], avgSus: ptr UncheckedArray[float], avgCumul: ptr UncheckedArray[float], avgM: ptr UncheckedArray[float],latticeZise: int): bool = 
-  let B = 0.0
+proc thread_func(task_id: int, c_len: int, cs: ptr UncheckedArray[float], avgHeat: ptr UncheckedArray[float], avgSus: ptr UncheckedArray[float], avgCumul: ptr UncheckedArray[float], avgM: ptr UncheckedArray[float],latticeZise: int): bool = 
   var rnd = initRand(task_id)
-  var lattice = newLattice(latticeZise, latticeZise, J, B, rnd)
   for i in 0 ..< c_len:
     let c = cs[i]
-
 
     let T = J*c/kb
     #let c = kb*T/J
       
     #echo "c = ", c
-    
-    lattice.T = T
+    let B = 0.0
+    var lattice = newLattice(latticeZise, latticeZise, J, B, T, rnd)
     #echo lattice.calcHamiltonian
-    let (M,msquare,m4,e,esquare) = isingHeatBath(lattice, rnd)
+    #echo "start: ", lattice.calcHamiltonian
+    let (M,msquare,m4,e,esquare) = ising(lattice, rnd)
+    #echo "end: ", lattice.calcHamiltonian
     #echo lattice.calcHamiltonian
     let specHeat = Heat_calc(e,esquare,T)
     let sus = Sus_calc(M,msquare,T)
@@ -41,15 +164,15 @@ proc main() =
 
 #SIZE 8
   echo "Running for size 8"
-  let c_len = 50
+  let c_len = 10
   var avgheat1: seq[float] = newSeq[float](c_len)
   var avgsus1: seq[float] = newSeq[float](c_len)
   var avgcumul1: seq[float] = newSeq[float](c_len)
   var avgM1: seq[float] = newSeq[float](c_len)
-  var cs = linspace(-6,-0.1,c_len).reversed
-  let times_run = 6 * 2000 # 1440 = 8h
+  var cs = linspace(2.0,10.0,c_len)
+  let times_run = 20
   var latticeZise = 8
-  var nthreads = countProcessors()
+  var nthreads = 1#countProcessors()
   var tp = Taskpool.new(num_threads = nthreads)
   var pendingTasks = newSeq[FlowVar[bool]](times_run)
   modLock.initLock
